c10aa75b3c32f7c75c632326a2881c0d37c51584
vasanthi1789 <146144372+vasanthi1789@users.noreply.github.com>
2023-09-26 22:33:53 -0500

    Update visualization5.r

--- question_2/src2/visualization5.r
+++ question_2/src2/visualization5.r
@@ -1,1 +1,7 @@
 
+library(readr)
+StudentsPerformance <- read_csv("C:/Users/venka/OneDrive/Desktop/vaasu/StudentsPerformance.csv")
+View(StudentsPerformance)
+attach(StudentsPerformance)
+v <- c(bachelorsdegree=11 ,somecollege=15,masterdegree=6,highschool=19,somehighschool=18,associatedegree=22)
+barplot(v,col="light blue")
